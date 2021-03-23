867dba153632a3b19835235c073368d654209fba
K Swa <kpswanson@gatech.edu>
2021-03-23 18:13:23 -0400

    update notes, add template app with empty component

---
+++ counter-app/app/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info" text="Hi!"></Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+    @import '~@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
