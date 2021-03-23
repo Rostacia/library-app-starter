65a641fba52660518e94a6391b3d0edb3b9bda5b
K Swa <kpswanson@gatech.edu>
2021-03-23 18:37:03 -0400

    updating notes

--- counter-app/app/components/Home.vue
+++ counter-app/app/components/Home.vue
@@ -4,8 +4,24 @@
             <Label text="Home"/>
         </ActionBar>
 
-        <GridLayout>
-            <Label class="info" text="Hi!"></Label>
+        <GridLayout columns="1*, 3*, 1*" rows="4*, 3*, 2*">
+            <Label class="info" row="0" col="1" text="Count goes here"></Label>
+            <Label row="=1" col="0">
+              <FormattedString>
+                <Span class="far" text="&plus;"></Span>
+              </FormattedString>
+            </Label>
+            <Label row="=1" col="2">
+              <FormattedString>
+                <Span class="far" text="&minus;"></Span>
+              </FormattedString>
+            </Label>
+            <Label row="2" col="1">
+              <FormattedString>
+                <Span class="far" text="&spin;"></Span>
+              </FormattedString>
+              <Button text="Reset"></Button>
+            </Label>
         </GridLayout>
     </Page>
 </template>
